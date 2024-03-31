21e488a3217505ff2eda32197eda5beb24b7ff08
boss <bossxlp@gmail.com>
2024-03-31 13:32:34 +0700

    feat: add get classby id service

--- src/routers/class.js
+++ src/routers/class.js
@@ -1,12 +1,26 @@
 // src/routes/itemRoutes.ts
 import express from "express";
 import { getClasses } from "../controllers/class";
-import { getClassDetails } from "../controllers/classDetails";
-
 
 const router = express.Router();
 
-router.get("/:id", getClassDetails)
+
+const classService = require('../services/classDetails');
+
+router.get("/:id", (req,res) => {
+    try {
+        const classId = req.params.id;
+        const classDetails = classService.getClass(classId);
+    
+        if (!classDetails) {
+          return res.status(404).json({ message: 'Class not found' });
+        }
+    
+        res.json(classDetails);
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
+})
 
 router.get("", getClasses);
 
