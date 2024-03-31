65a6e69783b83b48f0a904d8480b3e663a11a5b2
Under <un@odds.team>
2024-03-31 11:41:18 +0700

    chore: create get course controller

---
+++ src/controllers/getCourse.js
@@ -0,0 +1,15 @@
+export const getCourse = async (req, res) => {
+  const courseId = req.params.courseId;
+  const data = {
+    courseName: "Test Course",
+    duration: "1",
+    outline: "Fundamental: Why What and How of Design Thinking\n Workshop: How to facilitates and tools recommend\n Incremental: Do design thininkg by team",
+    images: ["a", "b"],
+    trainer: [{
+      name: "I AM",
+      image: "c",
+      experience: "trainer experience"
+    }]
+  };
+  res.send(data);
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import express from "express";
 import courseRouter from "./routers/course";
 import classDetailsRouter from "./routers/classDetails";
 import bodyParser from "body-parser";
+import { getCourse } from "./controllers/getCourse";
+
 const mongoose = require('mongoose');
 const EnrollmentModel = require('./models/enrollment');
-
-
 
 
 const app = express();
@@ -56,23 +56,7 @@ app.post("/register", async (req, res) => {
   res.send("register");
 });
 
-app.get("/course/:courseId", (req, res) => {
-  const courseId = req.params.courseId;
-  res.send({
-    courseName: "Test Course",
-    duration: "1",
-    maxParticipant: "20",
-    outline: "Fundamental: Why What and How of Design Thinking\n Workshop: How to facilitates and tools recommend\n Incremental: Do design thininkg by team",
-    agenda: "course agenda",
-    afterSaleService: "after sale service",
-    images: ["a", "b"],
-    trainer: [{
-      name: "I AM",
-      image: "c",
-      experience: "trainer experience"
-    }]
-  });
-});
+app.get("/course/:courseId", getCourse);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
