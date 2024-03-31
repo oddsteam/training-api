d938552771dc8850694eb9d6eb03a907154ce18c
boss <bossxlp@gmail.com>
2024-03-31 13:41:24 +0700

    feat: send back class details

--- src/routers/class.js
+++ src/routers/class.js
@@ -7,11 +7,10 @@ const router = express.Router();
 
 const classService = require('../services/classDetails');
 
-router.get("/:id", (req,res) => {
+router.get("/:id", async (req,res) => {
     try {
         const classId = req.params.id;
-        const classDetails = classService.getClass(classId);
-    
+        const classDetails = await classService.getClass(classId);
         if (!classDetails) {
           return res.status(404).json({ message: 'Class not found' });
         }
--- src/services/classDetails.js
+++ src/services/classDetails.js
@@ -2,7 +2,8 @@ const classDeatilsModel = require("../models/classDetails");
 export const getClass = async (id) => {
   try {
 
-    const data = await classDeatilsModel.findOne({ _id: id });;
+    const data = await classDeatilsModel.findOne({ _id: id });
+
     return data;
   } catch (error) {
     console.log(error);
