ee94eeb4d196578fcebc949fbc5d9d46909bf820
Firstnapat <firstnapat@odds.team>
2024-03-31 13:34:24 +0700

    feat: add reminder routes

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ var cors = require('cors');
 
 const mongoose = require('mongoose');
 
+import { createContent } from "./reminder/template.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -32,6 +33,27 @@ app.get("/healthcheck", (req, res) => {
 
 
 app.post("/register", enrollment);
+app.post("/register", async (req, res) => {
+  console.log(req.body);
+ try {
+  await EnrollmentModel.create(req.body);
+ } catch (error) {
+  console.log(error);
+ }
+  res.send("register");
+});
+app.get('/classes/:classId', (req, res) => {
+  res.send({
+    name: "SCRUM MASTER COMPASS", // class detail
+    description: `ตอนผู้สอนทั้ง 2 มาเป็น ScrumMaster ใหม่ ๆ เราพบช่วงเวลายากลำบากหลายครั้ง`,
+    place:"Geeky Base All Star",
+    googleMapUrl: "https://maps.app.goo.gl/UQKuNzejJNNwVB2K9",
+    startDate: "31/3/2024",
+    endDate: "31/3/2024",
+    startTime: "9:00",
+    endTime: "17:00",
+  })
+})
 
 app.get("/course/:courseId", getCourse);
 
