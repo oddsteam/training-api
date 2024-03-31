2369d51b866ae9a42845b5ece74fac2c95998f30
Under <un@odds.team>
2024-03-31 14:00:25 +0700

    chore: Get course detail from db

--- src/controllers/getCourse.js
+++ src/controllers/getCourse.js
@@ -1,15 +1,7 @@
+const CourseModel = require("../models/course");
+
 export const getCourse = async (req, res) => {
   const courseId = req.params.courseId;
-  const data = {
-    courseName: "Test Course",
-    duration: "1",
-    outline: "Fundamental: Why What and How of Design Thinking\n Workshop: How to facilitates and tools recommend\n Incremental: Do design thininkg by team",
-    images: ["a", "b"],
-    trainers: [{
-      name: "I AM",
-      image: "c",
-      experience: "trainer experience"
-    }]
-  };
+  const data = await CourseModel.findById(courseId);
   res.send(data);
 }
