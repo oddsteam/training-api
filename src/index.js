23494a51676218ba18943f0506b3283fe1e0f0a6
boss <bossxlp@gmail.com>
2024-03-31 11:49:16 +0700

    fix: add class details

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import express from "express";
 import courseRouter from "./routers/course";
 import classRouter from "./routers/class";
-import classDetailsRouter from "./routers/classDetails";
 import bodyParser from "body-parser";
 import { getCourse } from "./controllers/getCourse";
 
@@ -24,28 +23,8 @@ app.get("/", (req, res) => {
 
 app.use("/courses", courseRouter);
 app.use("/classes", classRouter);
-app.use("/classes", classDetailsRouter);
 app.get("/healthcheck", (req, res) => {
   res.send("Ok!");
-});
-
-app.get("/classes/:classId", (req, res) => {
-  res.send({
-    name: "SCRUM MASTER COMPASS", // class detail
-    description: `ตอนผู้สอนทั้ง 2 มาเป็น ScrumMaster ใหม่ ๆ เราพบช่วงเวลายากลำบากหลายครั้ง`,
-    place: "Geeky Base All Star",
-    googleMapUrl: "https://maps.app.goo.gl/UQKuNzejJNNwVB2K9",
-    startDate: "31/3/2024",
-    ensDate: "31/3/2024",
-    startTime: "9:00",
-    endTime: "17:00",
-  });
-});
-
-app.get("/classes/:classId/remaing", (req, res) => {
-  res.send({
-    remaing: 1, // จำนวนที่นั่งที่เหลืออยู่
-  });
 });
 
 app.post("/register", async (req, res) => {
--- src/routers/class.js
+++ src/routers/class.js
@@ -1,8 +1,12 @@
 // src/routes/itemRoutes.ts
 import express from "express";
 import { getClasses } from "../controllers/class";
+import { getClassDetails } from "../controllers/classDetails";
+
 
 const router = express.Router();
+
+router.get("/:id", getClassDetails)
 
 router.get("", getClasses);
 
