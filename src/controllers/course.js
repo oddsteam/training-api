06113d66bb7fde25b3bcb5a6a1e4da274ed978ec
Stamp <stamp@odds.team>
2024-03-31 11:22:59 +0700

    add data mock obj

--- src/controllers/course.js
+++ src/controllers/course.js
@@ -1,7 +1,63 @@
 export const getCourses = async (req, res) => {
   const data = [
     {
-      courseName: "Test Course",
+      courseName: "Test Course1",
+      outline: "course outline",
+      agenda: "course agenda",
+      afterSaleService: "after sale service",
+      images: ["a", "b"],
+      trainer: [
+        {
+          name: "I AM",
+          image: "c",
+          experience: "trainer experience",
+        },
+      ],
+    },
+    {
+      courseName: "Test Course2",
+      outline: "course outline",
+      agenda: "course agenda",
+      afterSaleService: "after sale service",
+      images: ["a", "b"],
+      trainer: [
+        {
+          name: "I AM",
+          image: "c",
+          experience: "trainer experience",
+        },
+      ],
+    },
+    {
+      courseName: "Test Course3",
+      outline: "course outline",
+      agenda: "course agenda",
+      afterSaleService: "after sale service",
+      images: ["a", "b"],
+      trainer: [
+        {
+          name: "I AM",
+          image: "c",
+          experience: "trainer experience",
+        },
+      ],
+    },
+    {
+      courseName: "Test Course4",
+      outline: "course outline",
+      agenda: "course agenda",
+      afterSaleService: "after sale service",
+      images: ["a", "b"],
+      trainer: [
+        {
+          name: "I AM",
+          image: "c",
+          experience: "trainer experience",
+        },
+      ],
+    },
+    {
+      courseName: "Test Course5",
       outline: "course outline",
       agenda: "course agenda",
       afterSaleService: "after sale service",
