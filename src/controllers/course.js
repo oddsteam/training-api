2c57c174e46624c71fd169fbbb291bcf08d96b2a
Stamp <stamp@odds.team>
2024-03-31 11:20:38 +0700

    update mockup data

--- src/controllers/course.js
+++ src/controllers/course.js
@@ -1,9 +1,18 @@
 export const getCourses = async (req, res) => {
   const data = [
     {
-      name: "course1",
-      tags: ["tag1", "tag2"],
-      detail: "description",
+      courseName: "Test Course",
+      outline: "course outline",
+      agenda: "course agenda",
+      afterSaleService: "after sale service",
+      images: ["a", "b"],
+      trainer: [
+        {
+          name: "I AM",
+          image: "c",
+          experience: "trainer experience",
+        },
+      ],
     },
   ];
   res.json({ data });
