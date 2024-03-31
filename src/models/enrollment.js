f8d4c1822d27cc35fa1503dc4289377a46ff8ccc
Wat <veerawat74@gmail.com>
2024-03-31 11:47:13 +0700

    fix

--- src/models/enrollment.js
+++ src/models/enrollment.js
@@ -5,6 +5,13 @@ const enrollmentSchema = new Schema({
   fullName: String,
   phoneNumber: String,
   company: String,
+  classId: String,
+  createDate: Date
+});
+
+enrollmentSchema.pre('save', function (next) {
+    this.createDate = new Date();
+    next();
 });
 
 const EnrollmentModel = mongoose.model('Enrollment', enrollmentSchema);
