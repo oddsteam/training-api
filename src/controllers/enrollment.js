8f47f0e28d36cdfa4a5361532f0514c690d18a4b
Wat <veerawat74@gmail.com>
2024-03-31 14:52:34 +0700

    fix

--- src/controllers/enrollment.js
+++ src/controllers/enrollment.js
@@ -26,7 +26,7 @@ function generateTemplate(name, className, place) {
 
   I hope this message finds you well. My name is Jane, and I'm excited to announce our upcoming class at ${place}, hosted by Odd-e Thailand.
 
-  To simplify the registration process, kindly provide your full name, email, and phone number. Once received, I will promptly add you to the list. For those opting for company-sponsored registration requiring an official receipt, please provide your company address for quotation purposes. However, if you're self-sponsored, you will benefit from a 20% discount and won't need an official receipt.
+  For those opting for company-sponsored registration requiring an official receipt, please provide your company address for quotation purposes. However, if you're self-sponsored, you will benefit from a 20% discount and won't need an official receipt.
 
   As you may already know, the regular price per person is 50,000 THB. Additionally, we're offering an early bird promotion known as the "Gang of Four" package, where four persons can enroll for a total of 150,000 THB.
 
