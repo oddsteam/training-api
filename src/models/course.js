1d8e400d689f7be7dbbd1c4b1c3ba0a1a975a490
Naravit Bunthap <naravitdansiriviroj@gmail.com>
2024-04-27 14:42:50 +0700

    - refactor code - prevent run seed double and happen duplicate seeded data - fix schema to relate with data

--- src/models/course.js
+++ src/models/course.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
-const TrainerModel = require("./trainer");
 const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, unique: true },
   courseName: String,
+  courseOutline: String,
+  courseOutlineEn: String,
+  createdAt: Date,
   duration: String,
-  outline: String,
   images: [String],
+  maxParticipant: String,
+  price: String,
   trainers: [String],
+  courseOutlineTh: String
+}, {
+  versionKey: false // You should be aware of the outcome after set to false
 });
 
 const CourseModel = mongoose.model("Course", courseSchema);
--- src/models/trainer.js
+++ src/models/trainer.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const trainerSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, unique: true },
   name: String,
+  image: String
+}, {
+  versionKey: false // You should be aware of the outcome after set to false
 });
 
 const TrainerModel = mongoose.model('Trainer', trainerSchema);
